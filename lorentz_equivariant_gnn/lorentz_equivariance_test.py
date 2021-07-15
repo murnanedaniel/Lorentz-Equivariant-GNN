b433f04661c80f9381e8fb2434625f126c6c7db7
jw5243 <jtwong71@gmail.com>
2021-07-15 15:03:10 -0400

    [ADD] Full lorentz equivariance test with boosts and rotations in all dimensions

--- lorentz_equivariant_gnn/lorentz_equivariance_test.py
+++ lorentz_equivariant_gnn/lorentz_equivariance_test.py
@@ -1,5 +1,5 @@
 from random import random
-from math import cos, sin, pi
+from math import cos, sin, pi, sqrt
 from copy import deepcopy
 import torch
 from lorentz_equivariant_gnn.legnn_model import LEGNN, get_edges_batch
@@ -32,8 +32,52 @@ def rotate_z(lorentz_vector, theta):
     return (rotation_matrix @ lorentz_vector.transpose(0, 1)).transpose(0, 1)
 
 
+def boost_x(lorentz_vector, gamma):
+    beta = sqrt(1 - 1 / (gamma ** 2))
+    boost_matrix = torch.tensor([[gamma, -beta * gamma, 0, 0],
+                                 [-beta * gamma, gamma, 0, 0],
+                                 [0, 0, 1, 0],
+                                 [0, 0, 0, 1]])
+    return (boost_matrix @ lorentz_vector.transpose(0, 1)).transpose(0, 1)
+
+
+def boost_y(lorentz_vector, gamma):
+    beta = sqrt(1 - 1 / (gamma ** 2))
+    boost_matrix = torch.tensor([[gamma, 0, -beta * gamma, 0],
+                                 [0, 1, 0, 0],
+                                 [-beta * gamma, 0, gamma, 0],
+                                 [0, 0, 0, 1]])
+    return (boost_matrix @ lorentz_vector.transpose(0, 1)).transpose(0, 1)
+
+
+def boost_z(lorentz_vector, gamma):
+    beta = sqrt(1 - 1 / (gamma ** 2))
+    boost_matrix = torch.tensor([[gamma, 0, 0, -beta * gamma],
+                                 [0, 1, 0, 0],
+                                 [0, 0, 1, 0],
+                                 [-beta * gamma, 0, 0, gamma]])
+    return (boost_matrix @ lorentz_vector.transpose(0, 1)).transpose(0, 1)
+
+
+def rotate(lorentz_vector, theta_x, theta_y, theta_z):
+    # Rotate in the order of x, y, and z
+    return rotate_z(rotate_y(rotate_x(lorentz_vector, theta_x), theta_y), theta_z)
+
+
+def boost(lorentz_vector, gamma_x, gamma_y, gamma_z):
+    # Rotate in the order of x, y, and z
+    return boost_z(boost_y(boost_x(lorentz_vector, gamma_x), gamma_y), gamma_z)
+
+
 if __name__ == '__main__':
-    test_angle = 2 * pi * random()
+    test_angle_x = 2 * pi * random()
+    test_angle_y = 2 * pi * random()
+    test_angle_z = 2 * pi * random()
+    test_boost_x = 2 * random() + 1
+    test_boost_y = 2 * random() + 1
+    test_boost_z = 2 * random() + 1
+
+    print(test_boost_x)
 
     # Dummy parameters
     batch_size = 1  # 8
@@ -50,18 +94,18 @@ if __name__ == '__main__':
     print("Input: " + str(x1))
 
     # Initialize LEGNN
-    legnn = LEGNN(input_feature_dim = n_feat, message_dim = 32, output_feature_dim = 1, edge_feature_dim = 1)
+    legnn = LEGNN(input_feature_dim = n_feat, message_dim = 32, output_feature_dim = 1, edge_feature_dim = 1, n_layers = 4)
 
     # Run LEGNN
 
     # First rotate the lorentz vector before passing through network
-    x1 = rotate_x(x1, test_angle)
+    x1 = boost(rotate(x1, test_angle_x, test_angle_y, test_angle_z), test_boost_x, test_boost_y, test_boost_z)
     h, x1 = legnn(h, x1, edges, edge_attr)
     print(x1)
 
     # Now rotate the lorentz vector after passing through the network
     h, x2 = legnn(h, x2, edges, edge_attr)
-    x2 = rotate_x(x2, test_angle)
+    x2 = boost(rotate(x2, test_angle_x, test_angle_y, test_angle_z), test_boost_x, test_boost_y, test_boost_z)
     print(x2)
 
     print(torch.isclose(x1, x2, atol = 1e-8, rtol = 1e-3))
